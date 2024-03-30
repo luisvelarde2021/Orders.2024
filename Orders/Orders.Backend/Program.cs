952145ca2722031f9cfbe860b24db7b1935f97fd
Luis Enrique Velarde Navarro <luisvelarde2021@hotmail.com>
2024-03-29 23:12:35 -0700

    add basic countries api controller

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -12,6 +12,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 var app = builder.Build();
 
+app.UseCors(x=> x
+.AllowAnyMethod()
+.AllowAnyHeader()
+.SetIsOriginAllowedToAllowWildcardSubdomains()
+.AllowCredentials());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
