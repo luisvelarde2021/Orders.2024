5e3361096e188ac8e88e9a2b5587bef9ae5b7918
Luis Enrique Velarde Navarro <luisvelarde2021@hotmail.com>
2024-03-30 19:41:14 -0700

    begin repository pattern

---
+++ Orders/Orders.Frontend/Repostories/HttpResponseWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+
+namespace Orders.Frontend.Repostories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no enconrado.";
+            }
+
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Se requiere estar logueado para ejecutar la operacion";
+            }
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "Sin permisos para ejecutar la operacion";
+            }
+
+            return "A ocurrido un error inesperado";
+        }
+    }
+}
